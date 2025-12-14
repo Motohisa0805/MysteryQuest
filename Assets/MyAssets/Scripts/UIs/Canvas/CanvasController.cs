ce9506fd96a01e468e47ee6abca5723c1a015de7
KusumeMotohisa <link080514@gmail.com>
2025-12-15 02:51:56 +0900

    シーン遷移UI追加

---
+++ Assets/MyAssets/Scripts/UIs/Canvas/CanvasController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace MyAssets
+{
+    //全シーンで使えるUIの処理を行う
+    public class CanvasController : MonoBehaviour
+    {
+        private BlackoutController mBlackoutController;
+
+
+        private void Awake()
+        {
+            mBlackoutController = FindAnyObjectByType<BlackoutController>();
+        }
+
+        private void Start()
+        {
+            if(mBlackoutController != null)
+            {
+                mBlackoutController.StartFadeIn();
+            }
+        }
+    }
+}
